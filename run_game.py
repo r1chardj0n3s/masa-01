55705df1aa337f53ac201771141c1a85e1f4048d
Richard Jones <r1chardj0n3s@gmail.com>
2020-09-20 12:35:17 +1000

    structure

--- run_game.py
+++ run_game.py
@@ -1,20 +1,25 @@
 import arcade
 import sys
 
-window = arcade.Window(1024, 768, "Junk Yard Wars")
-
-sprites = arcade.SpriteList()
-
-sprite = arcade.Sprite(':resources:images/alien/alienBlue_front.png', center_x=100, center_y=100)
-sprites.append(sprite)
-
-my_map = arcade.tilemap.read_tmx("data/first-map.tmx")
-
-background_list = arcade.tilemap.process_layer(my_map, 'Tile Layer 1', 1)
-
-arcade.start_render()
-background_list.draw()
-sprites.draw()
 
 
+class Game(arcade.Window):
+    def __init__(self):
+        super().__init__(1024, 768, "Junk Yard Wars")
+
+        self.sprites = arcade.SpriteList()
+
+        self.sprite = arcade.Sprite(':resources:images/alien/alienBlue_front.png', center_x=100, center_y=100, scale=.5)
+        self.sprites.append(self.sprite)
+
+        self.my_map = arcade.tilemap.read_tmx("data/first-map.tmx")
+
+        self.background_list = arcade.tilemap.process_layer(self.my_map, 'Tile Layer 1', .5)
+
+    def on_draw(self):
+        arcade.start_render()
+        self.background_list.draw()
+        self.sprites.draw()
+
+game = Game()
 arcade.run()
