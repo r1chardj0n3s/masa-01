33008d2c7c2cb883c271d962fefa3dfc2b1561ba
Richard Jones <r1chardj0n3s@gmail.com>
2020-09-20 12:20:24 +1000

    omg our tile map

--- run_game.py
+++ run_game.py
@@ -1,15 +1,19 @@
 import arcade
 import sys
 
-window = arcade.Window(800, 600, "Junk Yard Wars")
+window = arcade.Window(1024, 768, "Junk Yard Wars")
 
 sprites = arcade.SpriteList()
 
 sprite = arcade.Sprite(':resources:images/alien/alienBlue_front.png', center_x=100, center_y=100)
 sprites.append(sprite)
 
+my_map = arcade.tilemap.read_tmx("data/first-map.tmx")
+
+background_list = arcade.tilemap.process_layer(my_map, 'Tile Layer 1', 1)
 
 arcade.start_render()
+background_list.draw()
 sprites.draw()
 
 
