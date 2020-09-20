b6cde6d741f9f7950f301035cac7c96d143f3ea6
Richard Jones <r1chardj0n3s@gmail.com>
2020-09-20 13:03:22 +1000

    scaling and sizing

--- run_game.py
+++ run_game.py
@@ -1,20 +1,19 @@
 import arcade
-import sys
 
 keyboard = dict()
 
 class Game(arcade.Window):
     def __init__(self):
-        super().__init__(1024, 768, "Junk Yard Wars")
+        super().__init__(1280, 720, "Junk Yard Wars")
 
         self.sprites = arcade.SpriteList()
 
-        self.sprite = arcade.Sprite(':resources:images/alien/alienBlue_front.png', center_x=100, center_y=100, scale=.5)
+        self.sprite = arcade.Sprite(':resources:images/alien/alienBlue_front.png', center_x=100, center_y=100)
         self.sprites.append(self.sprite)
 
         self.my_map = arcade.tilemap.read_tmx("data/first-map.tmx")
 
-        self.background_list = arcade.tilemap.process_layer(self.my_map, 'Tile Layer 1', .5)
+        self.background_list = arcade.tilemap.process_layer(self.my_map, 'Tile Layer 1')
 
     def on_key_press(self, symbol, modifiers):
         keyboard[symbol] = True
