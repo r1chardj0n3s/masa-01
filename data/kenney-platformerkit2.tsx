2bd2705331e8b438316c52014eb94c15614d87c3
Richard Jones <r1chardj0n3s@gmail.com>
2020-09-20 15:34:02 +1000

    trigger layer in map

--- data/kenney-platformerkit2.tsx
+++ data/kenney-platformerkit2.tsx
@@ -1,5 +1,5 @@
 <?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.4" tiledversion="1.4.2" name="kenney-platformerkit2" tilewidth="512" tileheight="512" tilecount="2" columns="0">
+<tileset version="1.4" tiledversion="1.4.2" name="kenney-platformerkit2" tilewidth="512" tileheight="512" tilecount="3" columns="0">
  <tileoffset x="-192" y="170"/>
  <grid orientation="orthogonal" width="1" height="1"/>
  <tile id="416">
@@ -7,5 +7,8 @@
  </tile>
  <tile id="417">
   <image width="512" height="512" source="kenney_platformerkit2_isometric/blockCliffCorner_NW.png"/>
+ </tile>
+ <tile id="418">
+  <image width="512" height="512" source="kenney_platformerkit2_isometric/blockRoundedLarge_NE.png"/>
  </tile>
 </tileset>
--- run_game.py
+++ run_game.py
@@ -5,7 +5,7 @@ keyboard = dict()
 
 world = esper.World()
 
-PLAYER_SPEED = 100
+PLAYER_SPEED = 500
 
 
 class Sprite():
@@ -49,7 +49,7 @@ class VelocityPositionProcessor(esper.Processor):
 
 class SpriteProcessor(esper.Processor):
     def process(self, dt):
-        for ent, (sprite, position) in self.world.get_components(Sprite, Position):           
+        for ent, (sprite, position) in self.world.get_components(Sprite, Position):
             sprite.sprite.center_x = position.x
             sprite.sprite.center_y = position.y
 
@@ -65,14 +65,21 @@ class Game(arcade.Window):
 
         self.sprites = arcade.SpriteList()
 
-        sprite = arcade.Sprite(':resources:images/alien/alienBlue_front.png', center_x=100, center_y=100)
-        world.create_entity(PlayerControlled(), Velocity(0,0), Position(100, 100), Sprite(sprite))
-
+        sprite = arcade.Sprite(':resources:images/alien/alienBlue_front.png')
+        bee = arcade.Sprite(':resources:images/enemies/bee.png')
         self.sprites.append(sprite)   # TODO ECS me??
+        self.sprites.append(bee)   # TODO ECS me??
 
         self.my_map = arcade.tilemap.read_tmx("data/first-map.tmx")
+        map_height = self.my_map.map_size.height * self.my_map.tile_size[1]
+        for obj in arcade.tilemap.get_tilemap_layer(self.my_map, 'triggers').tiled_objects:
+            if obj.name == 'PLAYER_SPAWN':
+                world.create_entity(PlayerControlled(), Velocity(0,0), Position(obj.location.x, map_height-obj.location.y), Sprite(sprite))
+            if obj.name == 'ENEMY_SPAWN':
+                world.create_entity(Velocity(0,0), Position(obj.location.x, map_height-obj.location.y), Sprite(bee))
 
-        self.background_list = arcade.tilemap.process_layer(self.my_map, 'Tile Layer 1')
+        self.ground_list = arcade.tilemap.process_layer(self.my_map, 'ground')
+        self.foreground_list = arcade.tilemap.process_layer(self.my_map, 'foreground')
 
     def on_key_press(self, symbol, modifiers):
         keyboard[symbol] = True
@@ -87,8 +94,9 @@ class Game(arcade.Window):
 
     def on_draw(self):
         arcade.start_render()
-        self.background_list.draw()
+        self.ground_list.draw()
         self.sprites.draw()
+        self.foreground_list.draw()
 
 game = Game()
 arcade.run()
