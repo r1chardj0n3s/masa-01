657707ca852e71d901dece6ed11a712c2814176e
Richard Jones <r1chardj0n3s@gmail.com>
2020-09-20 12:41:49 +1000

    keyboard state

--- run_game.py
+++ run_game.py
@@ -1,7 +1,7 @@
 import arcade
 import sys
 
-
+keyboard = dict()
 
 class Game(arcade.Window):
     def __init__(self):
@@ -15,6 +15,24 @@ class Game(arcade.Window):
         self.my_map = arcade.tilemap.read_tmx("data/first-map.tmx")
 
         self.background_list = arcade.tilemap.process_layer(self.my_map, 'Tile Layer 1', .5)
+
+    def on_key_press(self, symbol, modifiers):
+        keyboard[symbol] = True
+        if symbol == arcade.key.ESCAPE:
+            arcade.close_window()
+
+    def on_key_release(self, symbol, modifiers):
+        del keyboard[symbol]
+
+    def on_update(self, dt):
+        if keyboard.get(arcade.key.UP):
+            self.sprite.center_y += 5
+        if keyboard.get(arcade.key.DOWN):
+            self.sprite.center_y -= 5
+        if keyboard.get(arcade.key.RIGHT):
+            self.sprite.center_x += 5
+        if keyboard.get(arcade.key.LEFT):
+            self.sprite.center_x -= 5
 
     def on_draw(self):
         arcade.start_render()
