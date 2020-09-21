17e3e6bc78d180fc5750b7234f59b0dfcfeea9fe
Travis Dixon <travis@growdata.com.au>
2020-09-22 06:15:31 +1000

    reposition space tiles

---
+++ data/space_terrain.tsx
@@ -0,0 +1,173 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.4" tiledversion="1.4.2" name="space_terrain" tilewidth="512" tileheight="512" tilecount="56" columns="0">
+ <tileoffset x="-192" y="170"/>
+ <grid orientation="orthogonal" width="1" height="1"/>
+ <tile id="0">
+  <image width="512" height="512" source="kenney_spacekit2_isometric/terrain_NE.png"/>
+ </tile>
+ <tile id="1">
+  <image width="512" height="512" source="kenney_spacekit2_isometric/terrain_NW.png"/>
+ </tile>
+ <tile id="2">
+  <image width="512" height="512" source="kenney_spacekit2_isometric/terrain_ramp_NE.png"/>
+ </tile>
+ <tile id="3">
+  <image width="512" height="512" source="kenney_spacekit2_isometric/terrain_ramp_NW.png"/>
+ </tile>
+ <tile id="4">
+  <image width="512" height="512" source="kenney_spacekit2_isometric/terrain_ramp_SE.png"/>
+ </tile>
+ <tile id="5">
+  <image width="512" height="512" source="kenney_spacekit2_isometric/terrain_ramp_SW.png"/>
+ </tile>
+ <tile id="6">
+  <image width="512" height="512" source="kenney_spacekit2_isometric/terrain_rampLarge_detailed_NE.png"/>
+ </tile>
+ <tile id="7">
+  <image width="512" height="512" source="kenney_spacekit2_isometric/terrain_rampLarge_detailed_NW.png"/>
+ </tile>
+ <tile id="8">
+  <image width="512" height="512" source="kenney_spacekit2_isometric/terrain_rampLarge_detailed_SE.png"/>
+ </tile>
+ <tile id="9">
+  <image width="512" height="512" source="kenney_spacekit2_isometric/terrain_rampLarge_detailed_SW.png"/>
+ </tile>
+ <tile id="10">
+  <image width="512" height="512" source="kenney_spacekit2_isometric/terrain_rampLarge_NE.png"/>
+ </tile>
+ <tile id="11">
+  <image width="512" height="512" source="kenney_spacekit2_isometric/terrain_rampLarge_NW.png"/>
+ </tile>
+ <tile id="12">
+  <image width="512" height="512" source="kenney_spacekit2_isometric/terrain_rampLarge_SE.png"/>
+ </tile>
+ <tile id="13">
+  <image width="512" height="512" source="kenney_spacekit2_isometric/terrain_rampLarge_SW.png"/>
+ </tile>
+ <tile id="14">
+  <image width="512" height="512" source="kenney_spacekit2_isometric/terrain_roadCorner_NE.png"/>
+ </tile>
+ <tile id="15">
+  <image width="512" height="512" source="kenney_spacekit2_isometric/terrain_roadCorner_NW.png"/>
+ </tile>
+ <tile id="16">
+  <image width="512" height="512" source="kenney_spacekit2_isometric/terrain_roadCorner_SE.png"/>
+ </tile>
+ <tile id="17">
+  <image width="512" height="512" source="kenney_spacekit2_isometric/terrain_roadCorner_SW.png"/>
+ </tile>
+ <tile id="18">
+  <image width="512" height="512" source="kenney_spacekit2_isometric/terrain_roadCross_NE.png"/>
+ </tile>
+ <tile id="19">
+  <image width="512" height="512" source="kenney_spacekit2_isometric/terrain_roadCross_NW.png"/>
+ </tile>
+ <tile id="20">
+  <image width="512" height="512" source="kenney_spacekit2_isometric/terrain_roadCross_SE.png"/>
+ </tile>
+ <tile id="21">
+  <image width="512" height="512" source="kenney_spacekit2_isometric/terrain_roadCross_SW.png"/>
+ </tile>
+ <tile id="22">
+  <image width="512" height="512" source="kenney_spacekit2_isometric/terrain_roadEnd_NE.png"/>
+ </tile>
+ <tile id="23">
+  <image width="512" height="512" source="kenney_spacekit2_isometric/terrain_roadEnd_NW.png"/>
+ </tile>
+ <tile id="24">
+  <image width="512" height="512" source="kenney_spacekit2_isometric/terrain_roadEnd_SE.png"/>
+ </tile>
+ <tile id="25">
+  <image width="512" height="512" source="kenney_spacekit2_isometric/terrain_roadEnd_SW.png"/>
+ </tile>
+ <tile id="26">
+  <image width="512" height="512" source="kenney_spacekit2_isometric/terrain_roadSplit_NE.png"/>
+ </tile>
+ <tile id="27">
+  <image width="512" height="512" source="kenney_spacekit2_isometric/terrain_roadSplit_NW.png"/>
+ </tile>
+ <tile id="28">
+  <image width="512" height="512" source="kenney_spacekit2_isometric/terrain_roadSplit_SE.png"/>
+ </tile>
+ <tile id="29">
+  <image width="512" height="512" source="kenney_spacekit2_isometric/terrain_roadSplit_SW.png"/>
+ </tile>
+ <tile id="30">
+  <image width="512" height="512" source="kenney_spacekit2_isometric/terrain_roadStraight_NE.png"/>
+ </tile>
+ <tile id="31">
+  <image width="512" height="512" source="kenney_spacekit2_isometric/terrain_roadStraight_NW.png"/>
+ </tile>
+ <tile id="32">
+  <image width="512" height="512" source="kenney_spacekit2_isometric/terrain_roadStraight_SE.png"/>
+ </tile>
+ <tile id="33">
+  <image width="512" height="512" source="kenney_spacekit2_isometric/terrain_roadStraight_SW.png"/>
+ </tile>
+ <tile id="34">
+  <image width="512" height="512" source="kenney_spacekit2_isometric/terrain_SE.png"/>
+ </tile>
+ <tile id="35">
+  <image width="512" height="512" source="kenney_spacekit2_isometric/terrain_side_NE.png"/>
+ </tile>
+ <tile id="36">
+  <image width="512" height="512" source="kenney_spacekit2_isometric/terrain_side_NW.png"/>
+ </tile>
+ <tile id="37">
+  <image width="512" height="512" source="kenney_spacekit2_isometric/terrain_side_SE.png"/>
+ </tile>
+ <tile id="38">
+  <image width="512" height="512" source="kenney_spacekit2_isometric/terrain_side_SW.png"/>
+ </tile>
+ <tile id="39">
+  <image width="512" height="512" source="kenney_spacekit2_isometric/terrain_sideCliff_NE.png"/>
+ </tile>
+ <tile id="40">
+  <image width="512" height="512" source="kenney_spacekit2_isometric/terrain_sideCliff_NW.png"/>
+ </tile>
+ <tile id="44">
+  <image width="512" height="512" source="kenney_spacekit2_isometric/terrain_sideCorner_NW.png"/>
+ </tile>
+ <tile id="45">
+  <image width="512" height="512" source="kenney_spacekit2_isometric/terrain_sideCorner_SE.png"/>
+ </tile>
+ <tile id="46">
+  <image width="512" height="512" source="kenney_spacekit2_isometric/terrain_sideCorner_SW.png"/>
+ </tile>
+ <tile id="47">
+  <image width="512" height="512" source="kenney_spacekit2_isometric/terrain_sideCornerInner_NE.png"/>
+ </tile>
+ <tile id="48">
+  <image width="512" height="512" source="kenney_spacekit2_isometric/terrain_sideCornerInner_NW.png"/>
+ </tile>
+ <tile id="49">
+  <image width="512" height="512" source="kenney_spacekit2_isometric/terrain_sideCornerInner_SE.png"/>
+ </tile>
+ <tile id="50">
+  <image width="512" height="512" source="kenney_spacekit2_isometric/terrain_sideCornerInner_SW.png"/>
+ </tile>
+ <tile id="51">
+  <image width="512" height="512" source="kenney_spacekit2_isometric/terrain_sideEnd_NE.png"/>
+ </tile>
+ <tile id="52">
+  <image width="512" height="512" source="kenney_spacekit2_isometric/terrain_sideEnd_NW.png"/>
+ </tile>
+ <tile id="53">
+  <image width="512" height="512" source="kenney_spacekit2_isometric/terrain_sideEnd_SE.png"/>
+ </tile>
+ <tile id="54">
+  <image width="512" height="512" source="kenney_spacekit2_isometric/terrain_sideEnd_SW.png"/>
+ </tile>
+ <tile id="55">
+  <image width="512" height="512" source="kenney_spacekit2_isometric/terrain_SW.png"/>
+ </tile>
+ <tile id="56">
+  <image width="512" height="512" source="kenney_spacekit2_isometric/terrain_sideCliff_SE.png"/>
+ </tile>
+ <tile id="57">
+  <image width="512" height="512" source="kenney_spacekit2_isometric/terrain_sideCliff_SW.png"/>
+ </tile>
+ <tile id="58">
+  <image width="512" height="512" source="kenney_spacekit2_isometric/terrain_sideCorner_NE.png"/>
+ </tile>
+</tileset>
