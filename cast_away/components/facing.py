7e1eabbe3de283bc06728e38b0691fdcd80a1508
Richard Jones <r1chardj0n3s@gmail.com>
2020-09-21 15:17:43 +1000

    fix facing to be 8 degrees

--- cast_away/components/facing.py
+++ cast_away/components/facing.py
@@ -1,19 +1,51 @@
 from .velocity import Velocity
 
 
+# kenney tiles have fake isometric, 2:1 pixel ratio of diagonals
+# the following transformation ensures hitting the controller diagonally
+# makes the character move on a "kenney-diagonal" line as drawn on the map
+DIMETRIC_FACTOR = 5.0 ** 0.5  # sqrt(5) aka diagonal of a triangle with sides 2, 1 
+SCALE_X = 1 / DIMETRIC_FACTOR
+SCALE_Y = 1 / (DIMETRIC_FACTOR * 2)
+
 class Facing:
+    NEUTRAL = "NEUTRAL"
     EAST = "EAST"
+    SOUTH_EAST = "SOUTH_EAST"
     SOUTH = "SOUTH"
+    SOUTH_WEST = "SOUTH_WEST"
     WEST = "WEST"
+    NORTH_WEST = "NORTH_WEST"
     NORTH = "NORTH"
+    NORTH_EAST = "NORTH_EAST"
+
+    CARDINALS = {
+        (0, 0): NEUTRAL,
+        (1, 0): EAST,
+        (1, -1): SOUTH_EAST,
+        (0, -1): SOUTH,
+        (-1, -1): SOUTH_WEST,
+        (-1, 0): WEST,
+        (-1, 1): NORTH_WEST,
+        (0, 1): NORTH,
+        (1, 1): NORTH_EAST,
+    }
 
     def __init__(self, direction):
-        self.direction = Facing.EAST
+        self.direction = Facing.NEUTRAL
+
+    def set_cardinals(self, left_right, up_down):
+        self.direction = self.CARDINALS[left_right, up_down]
 
     def velocity(self):
         return {
-            Facing.EAST: Velocity(1, 0),
-            Facing.WEST: Velocity(-1, 0),
-            Facing.NORTH: Velocity(0, 1),
-            Facing.SOUTH: Velocity(0, -1),
+            Facing.NEUTRAL: Velocity(0, 0),
+            Facing.EAST: Velocity(SCALE_X, 0),
+            Facing.SOUTH_EAST: Velocity(SCALE_X, -SCALE_Y),
+            Facing.SOUTH: Velocity(0, -SCALE_Y),
+            Facing.SOUTH_WEST: Velocity(-SCALE_X, -SCALE_Y),
+            Facing.WEST: Velocity(-SCALE_X, 0),
+            Facing.NORTH_WEST: Velocity(-SCALE_X, SCALE_Y),
+            Facing.NORTH: Velocity(0, SCALE_Y),
+            Facing.NORTH_EAST: Velocity(SCALE_X, SCALE_Y),
         }[self.direction]
--- cast_away/components/player.py
+++ cast_away/components/player.py
@@ -9,12 +9,6 @@ from .sprite import Sprite
 
 
 PLAYER_SPEED = 550
-# kenney tiles have fake isometric, 2:1 pixel ratio of diagonals
-# the following transformation ensures hitting the controller diagonally
-# makes the character move on a "kenney-diagonal" line as drawn on the map
-DIMETRIC_FACTOR = 5.0 ** 0.5  # sqrt(5) aka diagonal of a triangle with sides 2, 1 
-PLAYER_SPEED_X = PLAYER_SPEED / DIMETRIC_FACTOR
-PLAYER_SPEED_Y = PLAYER_SPEED / (DIMETRIC_FACTOR * 2)
 
 
 class PlayerControlled:
@@ -23,34 +17,22 @@ class PlayerControlled:
 
 class PlayerVelocityProcessor(esper.Processor):
     def process(self, dt):
-        for _, (pc, velocity) in self.world.get_components(PlayerControlled, Velocity):
-            dx = 0
-            dy = 0
-            if keyboard.state.get(arcade.key.UP):
-                dy += PLAYER_SPEED_Y
-            if keyboard.state.get(arcade.key.DOWN):
-                dy -= PLAYER_SPEED_Y
+        for ent, (pc, velocity, facing) in self.world.get_components(PlayerControlled, Velocity, Facing):
+            left_right = up_down = 0
             if keyboard.state.get(arcade.key.RIGHT):
-                dx += PLAYER_SPEED_X
+                left_right = 1
             if keyboard.state.get(arcade.key.LEFT):
-                dx -= PLAYER_SPEED_X
-            velocity.dx = dx
-            velocity.dy = dy
-            velocity.magnitude = PLAYER_SPEED
+                left_right -= 1
+            if keyboard.state.get(arcade.key.UP):
+                up_down = 1
+            if keyboard.state.get(arcade.key.DOWN):
+                up_down -= 1
+            facing.set_cardinals(left_right, up_down)
 
-class PlayerFacingProcessor(esper.Processor):
-    def process(self, dt):
-        for _, (pc, facing) in self.world.get_components(PlayerControlled, Facing):
-            if keyboard.state.get(arcade.key.UP):
-                facing.direction = Facing.NORTH
-            if keyboard.state.get(arcade.key.DOWN):
-                facing.direction = Facing.SOUTH
-            if keyboard.state.get(arcade.key.RIGHT):
-                facing.direction = Facing.EAST
-            if keyboard.state.get(arcade.key.LEFT):
-                facing.direction = Facing.WEST
+            velocity = facing.velocity()
+            velocity.magnitude = PLAYER_SPEED
+            self.world.add_component(ent, velocity)
 
 
 def init(world):
     world.add_processor(PlayerVelocityProcessor())
-    world.add_processor(PlayerFacingProcessor())
