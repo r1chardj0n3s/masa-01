79c9886c4d8e1639e790eb48186340c27526a966
Travis Dixon <travis@growdata.com.au>
2020-09-21 15:30:10 +1000

    spike level

---
+++ data/kenney-props.tsx
@@ -0,0 +1,341 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.4" tiledversion="1.4.2" name="kenney-props" tilewidth="512" tileheight="512" tilecount="112" columns="0">
+ <tileoffset x="-192" y="170"/>
+ <grid orientation="orthogonal" width="1" height="1"/>
+ <tile id="0">
+  <image width="512" height="512" source="kenney_platformerkit2_isometric/arrow_NE.png"/>
+ </tile>
+ <tile id="1">
+  <image width="512" height="512" source="kenney_platformerkit2_isometric/arrow_NW.png"/>
+ </tile>
+ <tile id="2">
+  <image width="512" height="512" source="kenney_platformerkit2_isometric/arrow_SE.png"/>
+ </tile>
+ <tile id="3">
+  <image width="512" height="512" source="kenney_platformerkit2_isometric/arrow_SW.png"/>
+ </tile>
+ <tile id="4">
+  <image width="512" height="512" source="kenney_platformerkit2_isometric/arrows_NE.png"/>
+ </tile>
+ <tile id="5">
+  <image width="512" height="512" source="kenney_platformerkit2_isometric/arrows_NW.png"/>
+ </tile>
+ <tile id="6">
+  <image width="512" height="512" source="kenney_platformerkit2_isometric/arrows_SE.png"/>
+ </tile>
+ <tile id="7">
+  <image width="512" height="512" source="kenney_platformerkit2_isometric/arrows_SW.png"/>
+ </tile>
+ <tile id="8">
+  <image width="512" height="512" source="kenney_platformerkit2_isometric/barrel_NE.png"/>
+ </tile>
+ <tile id="9">
+  <image width="512" height="512" source="kenney_platformerkit2_isometric/barrel_NW.png"/>
+ </tile>
+ <tile id="10">
+  <image width="512" height="512" source="kenney_platformerkit2_isometric/barrel_SE.png"/>
+ </tile>
+ <tile id="11">
+  <image width="512" height="512" source="kenney_platformerkit2_isometric/barrel_SW.png"/>
+ </tile>
+ <tile id="12">
+  <image width="512" height="512" source="kenney_platformerkit2_isometric/buttonRound_NE.png"/>
+ </tile>
+ <tile id="13">
+  <image width="512" height="512" source="kenney_platformerkit2_isometric/buttonRound_NW.png"/>
+ </tile>
+ <tile id="14">
+  <image width="512" height="512" source="kenney_platformerkit2_isometric/buttonRound_SE.png"/>
+ </tile>
+ <tile id="15">
+  <image width="512" height="512" source="kenney_platformerkit2_isometric/buttonRound_SW.png"/>
+ </tile>
+ <tile id="16">
+  <image width="512" height="512" source="kenney_platformerkit2_isometric/buttonSquare_NE.png"/>
+ </tile>
+ <tile id="17">
+  <image width="512" height="512" source="kenney_platformerkit2_isometric/buttonSquare_NW.png"/>
+ </tile>
+ <tile id="18">
+  <image width="512" height="512" source="kenney_platformerkit2_isometric/buttonSquare_SE.png"/>
+ </tile>
+ <tile id="19">
+  <image width="512" height="512" source="kenney_platformerkit2_isometric/buttonSquare_SW.png"/>
+ </tile>
+ <tile id="20">
+  <image width="512" height="512" source="kenney_platformerkit2_isometric/chest_NE.png"/>
+ </tile>
+ <tile id="21">
+  <image width="512" height="512" source="kenney_platformerkit2_isometric/chest_NW.png"/>
+ </tile>
+ <tile id="22">
+  <image width="512" height="512" source="kenney_platformerkit2_isometric/chest_SE.png"/>
+ </tile>
+ <tile id="23">
+  <image width="512" height="512" source="kenney_platformerkit2_isometric/chest_SW.png"/>
+ </tile>
+ <tile id="24">
+  <image width="512" height="512" source="kenney_platformerkit2_isometric/coinBronze_NE.png"/>
+ </tile>
+ <tile id="25">
+  <image width="512" height="512" source="kenney_platformerkit2_isometric/coinBronze_NW.png"/>
+ </tile>
+ <tile id="26">
+  <image width="512" height="512" source="kenney_platformerkit2_isometric/coinBronze_SE.png"/>
+ </tile>
+ <tile id="27">
+  <image width="512" height="512" source="kenney_platformerkit2_isometric/coinBronze_SW.png"/>
+ </tile>
+ <tile id="28">
+  <image width="512" height="512" source="kenney_platformerkit2_isometric/coinGold_NE.png"/>
+ </tile>
+ <tile id="29">
+  <image width="512" height="512" source="kenney_platformerkit2_isometric/coinGold_NW.png"/>
+ </tile>
+ <tile id="30">
+  <image width="512" height="512" source="kenney_platformerkit2_isometric/coinGold_SE.png"/>
+ </tile>
+ <tile id="31">
+  <image width="512" height="512" source="kenney_platformerkit2_isometric/coinGold_SW.png"/>
+ </tile>
+ <tile id="32">
+  <image width="512" height="512" source="kenney_platformerkit2_isometric/coinSilver_NE.png"/>
+ </tile>
+ <tile id="33">
+  <image width="512" height="512" source="kenney_platformerkit2_isometric/coinSilver_NW.png"/>
+ </tile>
+ <tile id="34">
+  <image width="512" height="512" source="kenney_platformerkit2_isometric/coinSilver_SE.png"/>
+ </tile>
+ <tile id="35">
+  <image width="512" height="512" source="kenney_platformerkit2_isometric/coinSilver_SW.png"/>
+ </tile>
+ <tile id="36">
+  <image width="512" height="512" source="kenney_platformerkit2_isometric/crate_NE.png"/>
+ </tile>
+ <tile id="37">
+  <image width="512" height="512" source="kenney_platformerkit2_isometric/crate_NW.png"/>
+ </tile>
+ <tile id="38">
+  <image width="512" height="512" source="kenney_platformerkit2_isometric/crate_SE.png"/>
+ </tile>
+ <tile id="39">
+  <image width="512" height="512" source="kenney_platformerkit2_isometric/crate_SW.png"/>
+ </tile>
+ <tile id="40">
+  <image width="512" height="512" source="kenney_platformerkit2_isometric/crateItem_NE.png"/>
+ </tile>
+ <tile id="41">
+  <image width="512" height="512" source="kenney_platformerkit2_isometric/crateItem_NW.png"/>
+ </tile>
+ <tile id="42">
+  <image width="512" height="512" source="kenney_platformerkit2_isometric/crateItem_SE.png"/>
+ </tile>
+ <tile id="43">
+  <image width="512" height="512" source="kenney_platformerkit2_isometric/crateItem_SW.png"/>
+ </tile>
+ <tile id="44">
+  <image width="512" height="512" source="kenney_platformerkit2_isometric/crateItemStrong_NE.png"/>
+ </tile>
+ <tile id="45">
+  <image width="512" height="512" source="kenney_platformerkit2_isometric/crateItemStrong_NW.png"/>
+ </tile>
+ <tile id="46">
+  <image width="512" height="512" source="kenney_platformerkit2_isometric/crateItemStrong_SE.png"/>
+ </tile>
+ <tile id="47">
+  <image width="512" height="512" source="kenney_platformerkit2_isometric/crateItemStrong_SW.png"/>
+ </tile>
+ <tile id="48">
+  <image width="512" height="512" source="kenney_platformerkit2_isometric/crateStrong_NE.png"/>
+ </tile>
+ <tile id="49">
+  <image width="512" height="512" source="kenney_platformerkit2_isometric/crateStrong_NW.png"/>
+ </tile>
+ <tile id="50">
+  <image width="512" height="512" source="kenney_platformerkit2_isometric/crateStrong_SE.png"/>
+ </tile>
+ <tile id="51">
+  <image width="512" height="512" source="kenney_platformerkit2_isometric/crateStrong_SW.png"/>
+ </tile>
+ <tile id="52">
+  <image width="512" height="512" source="kenney_platformerkit2_isometric/flag_NE.png"/>
+ </tile>
+ <tile id="53">
+  <image width="512" height="512" source="kenney_platformerkit2_isometric/flag_NW.png"/>
+ </tile>
+ <tile id="54">
+  <image width="512" height="512" source="kenney_platformerkit2_isometric/flag_SE.png"/>
+ </tile>
+ <tile id="55">
+  <image width="512" height="512" source="kenney_platformerkit2_isometric/flag_SW.png"/>
+ </tile>
+ <tile id="56">
+  <image width="512" height="512" source="kenney_platformerkit2_isometric/flowers_NE.png"/>
+ </tile>
+ <tile id="57">
+  <image width="512" height="512" source="kenney_platformerkit2_isometric/flowers_NW.png"/>
+ </tile>
+ <tile id="58">
+  <image width="512" height="512" source="kenney_platformerkit2_isometric/flowers_SE.png"/>
+ </tile>
+ <tile id="59">
+  <image width="512" height="512" source="kenney_platformerkit2_isometric/flowers_SW.png"/>
+ </tile>
+ <tile id="60">
+  <image width="512" height="512" source="kenney_platformerkit2_isometric/flowersLow_NE.png"/>
+ </tile>
+ <tile id="61">
+  <image width="512" height="512" source="kenney_platformerkit2_isometric/flowersLow_NW.png"/>
+ </tile>
+ <tile id="62">
+  <image width="512" height="512" source="kenney_platformerkit2_isometric/flowersLow_SE.png"/>
+ </tile>
+ <tile id="63">
+  <image width="512" height="512" source="kenney_platformerkit2_isometric/flowersLow_SW.png"/>
+ </tile>
+ <tile id="64">
+  <image width="512" height="512" source="kenney_platformerkit2_isometric/heart_NE.png"/>
+ </tile>
+ <tile id="65">
+  <image width="512" height="512" source="kenney_platformerkit2_isometric/heart_NW.png"/>
+ </tile>
+ <tile id="66">
+  <image width="512" height="512" source="kenney_platformerkit2_isometric/heart_SE.png"/>
+ </tile>
+ <tile id="67">
+  <image width="512" height="512" source="kenney_platformerkit2_isometric/heart_SW.png"/>
+ </tile>
+ <tile id="68">
+  <image width="512" height="512" source="kenney_platformerkit2_isometric/jewel_NE.png"/>
+ </tile>
+ <tile id="69">
+  <image width="512" height="512" source="kenney_platformerkit2_isometric/jewel_NW.png"/>
+ </tile>
+ <tile id="70">
+  <image width="512" height="512" source="kenney_platformerkit2_isometric/jewel_SE.png"/>
+ </tile>
+ <tile id="71">
+  <image width="512" height="512" source="kenney_platformerkit2_isometric/jewel_SW.png"/>
+ </tile>
+ <tile id="72">
+  <image width="512" height="512" source="kenney_platformerkit2_isometric/key_NE.png"/>
+ </tile>
+ <tile id="73">
+  <image width="512" height="512" source="kenney_platformerkit2_isometric/key_NW.png"/>
+ </tile>
+ <tile id="74">
+  <image width="512" height="512" source="kenney_platformerkit2_isometric/key_SE.png"/>
+ </tile>
+ <tile id="75">
+  <image width="512" height="512" source="kenney_platformerkit2_isometric/key_SW.png"/>
+ </tile>
+ <tile id="76">
+  <image width="512" height="512" source="kenney_platformerkit2_isometric/lever_NE.png"/>
+ </tile>
+ <tile id="77">
+  <image width="512" height="512" source="kenney_platformerkit2_isometric/lever_NW.png"/>
+ </tile>
+ <tile id="78">
+  <image width="512" height="512" source="kenney_platformerkit2_isometric/lever_SE.png"/>
+ </tile>
+ <tile id="79">
+  <image width="512" height="512" source="kenney_platformerkit2_isometric/lever_SW.png"/>
+ </tile>
+ <tile id="80">
+  <image width="512" height="512" source="kenney_platformerkit2_isometric/mushrooms_NE.png"/>
+ </tile>
+ <tile id="81">
+  <image width="512" height="512" source="kenney_platformerkit2_isometric/mushrooms_NW.png"/>
+ </tile>
+ <tile id="82">
+  <image width="512" height="512" source="kenney_platformerkit2_isometric/mushrooms_SE.png"/>
+ </tile>
+ <tile id="83">
+  <image width="512" height="512" source="kenney_platformerkit2_isometric/mushrooms_SW.png"/>
+ </tile>
+ <tile id="84">
+  <image width="512" height="512" source="kenney_platformerkit2_isometric/plant_NE.png"/>
+ </tile>
+ <tile id="85">
+  <image width="512" height="512" source="kenney_platformerkit2_isometric/plant_NW.png"/>
+ </tile>
+ <tile id="86">
+  <image width="512" height="512" source="kenney_platformerkit2_isometric/plant_SE.png"/>
+ </tile>
+ <tile id="87">
+  <image width="512" height="512" source="kenney_platformerkit2_isometric/plant_SW.png"/>
+ </tile>
+ <tile id="88">
+  <image width="512" height="512" source="kenney_platformerkit2_isometric/rocks_NE.png"/>
+ </tile>
+ <tile id="89">
+  <image width="512" height="512" source="kenney_platformerkit2_isometric/rocks_NW.png"/>
+ </tile>
+ <tile id="90">
+  <image width="512" height="512" source="kenney_platformerkit2_isometric/rocks_SE.png"/>
+ </tile>
+ <tile id="91">
+  <image width="512" height="512" source="kenney_platformerkit2_isometric/rocks_SW.png"/>
+ </tile>
+ <tile id="92">
+  <image width="512" height="512" source="kenney_platformerkit2_isometric/saw_NE.png"/>
+ </tile>
+ <tile id="93">
+  <image width="512" height="512" source="kenney_platformerkit2_isometric/saw_NW.png"/>
+ </tile>
+ <tile id="94">
+  <image width="512" height="512" source="kenney_platformerkit2_isometric/saw_SE.png"/>
+ </tile>
+ <tile id="95">
+  <image width="512" height="512" source="kenney_platformerkit2_isometric/saw_SW.png"/>
+ </tile>
+ <tile id="96">
+  <image width="512" height="512" source="kenney_platformerkit2_isometric/sign_NE.png"/>
+ </tile>
+ <tile id="97">
+  <image width="512" height="512" source="kenney_platformerkit2_isometric/sign_NW.png"/>
+ </tile>
+ <tile id="98">
+  <image width="512" height="512" source="kenney_platformerkit2_isometric/sign_SE.png"/>
+ </tile>
+ <tile id="99">
+  <image width="512" height="512" source="kenney_platformerkit2_isometric/sign_SW.png"/>
+ </tile>
+ <tile id="100">
+  <image width="512" height="512" source="kenney_platformerkit2_isometric/spikes_NE.png"/>
+ </tile>
+ <tile id="101">
+  <image width="512" height="512" source="kenney_platformerkit2_isometric/spikes_NW.png"/>
+ </tile>
+ <tile id="102">
+  <image width="512" height="512" source="kenney_platformerkit2_isometric/spikes_SE.png"/>
+ </tile>
+ <tile id="103">
+  <image width="512" height="512" source="kenney_platformerkit2_isometric/spikes_SW.png"/>
+ </tile>
+ <tile id="104">
+  <image width="512" height="512" source="kenney_platformerkit2_isometric/spikesHidden_NE.png"/>
+ </tile>
+ <tile id="105">
+  <image width="512" height="512" source="kenney_platformerkit2_isometric/spikesHidden_NW.png"/>
+ </tile>
+ <tile id="106">
+  <image width="512" height="512" source="kenney_platformerkit2_isometric/spikesHidden_SE.png"/>
+ </tile>
+ <tile id="107">
+  <image width="512" height="512" source="kenney_platformerkit2_isometric/spikesHidden_SW.png"/>
+ </tile>
+ <tile id="108">
+  <image width="512" height="512" source="kenney_platformerkit2_isometric/spikesLarge_NE.png"/>
+ </tile>
+ <tile id="109">
+  <image width="512" height="512" source="kenney_platformerkit2_isometric/spikesLarge_NW.png"/>
+ </tile>
+ <tile id="110">
+  <image width="512" height="512" source="kenney_platformerkit2_isometric/spikesLarge_SE.png"/>
+ </tile>
+ <tile id="111">
+  <image width="512" height="512" source="kenney_platformerkit2_isometric/spikesLarge_SW.png"/>
+ </tile>
+</tileset>
