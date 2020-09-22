a53cdb0d3e39667d71e9572ca8bf2b524be2f880
Richard Jones <r1chardj0n3s@gmail.com>
2020-09-22 15:32:37 +1000

    allow maps to extend offscreen

--- cast_away/hud.py
+++ cast_away/hud.py
@@ -1,11 +1,11 @@
 import arcade
 
-from .components.draw_layer import DrawLayer
 from .components.health_display import HealthDisplay
+from cast_away.components.draw_layer import HUDLayer
 
 def add_health_hud(world):
     sprite_list = arcade.SpriteList()
     sprite_list.append(arcade.Sprite('data/kenney_platformerpack_redux/HUD/hudHeart_full.png', center_x=1150, center_y=50, scale=.5))
     sprite_list.append(arcade.Sprite('data/kenney_platformerpack_redux/HUD/hudHeart_full.png', center_x=1200, center_y=50, scale=.5))
     sprite_list.append(arcade.Sprite('data/kenney_platformerpack_redux/HUD/hudHeart_full.png', center_x=1250, center_y=50, scale=.5))
-    world.create_entity(HealthDisplay(sprite_list), DrawLayer(9999, sprite_list))
+    world.create_entity(HealthDisplay(sprite_list), HUDLayer(sprite_list))
--- cast_away/window.py
+++ cast_away/window.py
@@ -10,6 +10,7 @@ from .components.input_source import KeyboardInputSource
 
 from .hud import add_health_hud
 from .menu import Menu
+from cast_away.components.draw_layer import HUDLayer
 
 class Game(arcade.Window):
     def __init__(self, map_name="1-movement"):
@@ -35,6 +36,8 @@ class Game(arcade.Window):
 
     def on_draw(self):
         arcade.start_render()
+
+        arcade.set_viewport(64, 1280+64, -32, 720-32)
         draw_layers = [c for (_, c) in self.world.get_component(DrawLayer)]
         draw_layers.sort(key=lambda layer: layer.priority)
         for layer in draw_layers:
@@ -56,6 +59,9 @@ class Game(arcade.Window):
                     debug.size
                 )
         
+        arcade.set_viewport(0, 1280, 0, 720)
+        for _, layer in self.world.get_component(HUDLayer):
+            layer.draw()
         if self.menu.show:
             self.menu.draw()
         
