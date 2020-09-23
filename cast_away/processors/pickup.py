d144f3b85b4c8b8ffaf4844fe90c117ad3f85f28
Travis Dixon <travis@growdata.com.au>
2020-09-23 15:36:07 +1000

    filter collision events to only care about entities that are PickupSpawners

--- cast_away/processors/pickup.py
+++ cast_away/processors/pickup.py
@@ -10,11 +10,12 @@ from cast_away.event_dispatch import COLLISION, register_listener
 
 def collision(world, message):
     pickup_ent, inventory_ent = message.payload
-    spawner = world.component_for_entity(pickup_ent, PickupSpawner)
-    item = create_inventory_item(world, inventory_ent, spawner.hud_image, spawner.inventory_item_component_classes)
-    if item is not None:
-        print(f"deleting {pickup_ent} FIXME! pickup processor(message handler)")
-        world.delete_entity(pickup_ent)
+    if world.has_component(pickup_ent, PickupSpawner):
+        spawner = world.component_for_entity(pickup_ent, PickupSpawner)
+        item = create_inventory_item(world, inventory_ent, spawner.hud_image, spawner.inventory_item_component_classes)
+        if item is not None:
+            print(f"deleting {pickup_ent} FIXME! pickup processor(message handler)")
+            world.delete_entity(pickup_ent)
 
 def init(world):
     register_listener(COLLISION, collision)
