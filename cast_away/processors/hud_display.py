95e6089475dcb7dd35c9f1d563f5103595359628
Richard Jones <r1chardj0n3s@gmail.com>
2020-09-23 21:11:16 +1000

    fix pickup

--- cast_away/processors/hud_display.py
+++ cast_away/processors/hud_display.py
@@ -8,7 +8,8 @@ from cast_away.components.player import Player
 from cast_away.components.inventory import Inventory, InventoryItem
 
 from cast_away.components.hud.hud_layer import HUDLayer
-from cast_away.entities.hud.inventory_display import inventory_hud_sprite, x_for_hud_sprite
+from cast_away.entities.hud.inventory_display import inventory_hud_sprite
+from cast_away.components.multiplayer_identifier import MultiplayerIdentifier, COLOURS
 
 FULL = "data/kenney_platformerpack_redux/HUD/hudHeart_full.png"
 EMPTY = "data/kenney_platformerpack_redux/HUD/hudHeart_empty.png"
@@ -28,16 +29,19 @@ class InventoryDisplayProcessor(esper.Processor):
     def process(self, dt):
         for _, (display, hud_layer) in self.world.get_components(InventoryHudDisplay, HUDLayer):
             sprite_list = hud_layer.drawable.item_sprite_list
-            def set_sprite_at(i, image):
+            def set_sprite_at(i, base_x, image):
                 if i < len(sprite_list):
                     sprite_list[i].texture = arcade.load_texture(image)
                 else:
-                    sprite_list.append(inventory_hud_sprite(image, i, scale=0.5))
+                    sprite_list.append(inventory_hud_sprite(image, i, base_x, scale=0.25))
 
             inventory = self.world.component_for_entity(display.player_entity, Inventory)
+            mp = self.world.component_for_entity(display.player_entity, MultiplayerIdentifier)
             for i, item_entity in enumerate(inventory.items):
                 inventoryItem = self.world.component_for_entity(item_entity, InventoryItem)
-                set_sprite_at(i, inventoryItem.hud_image)
+                index = COLOURS.index(mp.colour)
+                base_x = index * 300
+                set_sprite_at(i, base_x, inventoryItem.hud_image)
             while len(inventory.items) < len(sprite_list):
                 sprite_list.pop()
 
