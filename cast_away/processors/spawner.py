7066d614dff792bf3c55805cdcf29212683fe481
Travis Dixon <travis@growdata.com.au>
2020-09-24 07:46:38 +1000

    fix enemy spawner

--- cast_away/processors/spawner.py
+++ cast_away/processors/spawner.py
@@ -10,11 +10,14 @@ from cast_away.entities.enemy import create_enemy
 
 class EnemySpawnProcessor(esper.Processor):
     def process(self, dt):
-        for ent, (es, position, level) in self.world.get_components(EnemySpawner, Position, Level):
+        for ent, (es, position) in self.world.get_components(EnemySpawner, Position):
+            level = self.world.component_for_entity(position.level, Level)
+            if not level.loaded:
+                continue
             if es.spawning:
                 if self.world.has_component(ent, Timeout):
                     continue
-                create_enemy(self.world, ent, position, level)
+                create_enemy(self.world, ent, position)
                 es.spawning = False
 
             for _, enemy in self.world.get_component(Enemy):
