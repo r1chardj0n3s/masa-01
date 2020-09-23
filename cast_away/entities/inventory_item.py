061b0a2e083ae89aa0570f5b45685cf0fd3c9a53
Travis Dixon <travis@growdata.com.au>
2020-09-23 13:55:42 +1000

    inventory size limit

--- cast_away/entities/inventory_item.py
+++ cast_away/entities/inventory_item.py
@@ -1,10 +1,13 @@
 from cast_away.components.inventory import InventoryItem, Inventory
 
+INVENTORY_SIZE = 3
+
 def create_inventory_item(world, owner_ent, hud_image, inventory_item_component_classes):
     inventory = world.component_for_entity(owner_ent, Inventory)
-    item = world.create_entity(InventoryItem(owner_ent=owner_ent, hud_image=hud_image), *[c() for c in inventory_item_component_classes])
-    inventory.items.append(item)
-    return item
+    if len(inventory.items) < INVENTORY_SIZE:
+        item = world.create_entity(InventoryItem(owner_ent=owner_ent, hud_image=hud_image), *[c() for c in inventory_item_component_classes])
+        inventory.items.append(item)
+        return item
 
 def drop_inventory_item(world, item_ent):
     item_comp = world.component_for_entity(item_ent, InventoryItem)
--- cast_away/processors/pickup.py
+++ cast_away/processors/pickup.py
@@ -12,8 +12,9 @@ def collision(world, message):
     pickup_ent, inventory_ent = message.payload
     spawner = world.component_for_entity(pickup_ent, PickupSpawner)
     item = create_inventory_item(world, inventory_ent, spawner.hud_image, spawner.inventory_item_component_classes)
-    print(f"deleting {pickup_ent} FIXME! pickup processor(message handler)")
-    world.delete_entity(pickup_ent)
+    if item is not None:
+        print(f"deleting {pickup_ent} FIXME! pickup processor(message handler)")
+        world.delete_entity(pickup_ent)
 
 def init(world):
     register_listener(COLLISION, collision)
