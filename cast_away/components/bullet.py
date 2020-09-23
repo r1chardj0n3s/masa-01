8b4c8659493028e79aa18041f4dfb686fb966d76
Richard Jones <r1chardj0n3s@gmail.com>
2020-09-23 16:05:47 +1000

    small fixes

--- cast_away/components/bullet.py
+++ cast_away/components/bullet.py
@@ -3,5 +3,5 @@ from dataclasses import dataclass
 
 @dataclass
 class Bullet:
-    lifespan: int
+    lifespan: float
     target: object
--- cast_away/processors/star_thrower.py
+++ cast_away/processors/star_thrower.py
@@ -18,7 +18,6 @@ class ShootingProcessor(esper.Processor):
     def process(self, dt):
         for item_ent, (item, st) in self.world.get_components(InventoryItem, StarThrower):
             if self.world.has_component(item_ent, Timeout):
-                print(f"timout: {self.world.component_for_entity(item_ent, Timeout)}")
                 continue
 
             pc = self.world.component_for_entity(item.owner_ent, Player)
