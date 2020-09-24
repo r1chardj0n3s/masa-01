660b1b94fe2516c968d47188b50736d2b76c52b7
Richard Jones <r1chardj0n3s@gmail.com>
2020-09-24 15:06:39 +1000

    new junkyard art

---
+++ data/junkyard-props.tsx
@@ -0,0 +1,161 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.4" tiledversion="1.4.2" name="junkyard-props" tilewidth="512" tileheight="512" tilecount="52" columns="0">
+ <tileoffset x="-192" y="170"/>
+ <grid orientation="orthogonal" width="1" height="1"/>
+ <tile id="0">
+  <image width="512" height="512" source="junkyard_isometriclandscape/crater_NE.png"/>
+ </tile>
+ <tile id="1">
+  <image width="512" height="512" source="junkyard_isometriclandscape/crater_NW.png"/>
+ </tile>
+ <tile id="2">
+  <image width="512" height="512" source="junkyard_isometriclandscape/crater_SE.png"/>
+ </tile>
+ <tile id="3">
+  <image width="512" height="512" source="junkyard_isometriclandscape/crater_SW.png"/>
+ </tile>
+ <tile id="4">
+  <image width="512" height="512" source="junkyard_isometriclandscape/craterLarge_NE.png"/>
+ </tile>
+ <tile id="5">
+  <image width="512" height="512" source="junkyard_isometriclandscape/craterLarge_NW.png"/>
+ </tile>
+ <tile id="6">
+  <image width="512" height="512" source="junkyard_isometriclandscape/craterLarge_SE.png"/>
+ </tile>
+ <tile id="7">
+  <image width="512" height="512" source="junkyard_isometriclandscape/craterLarge_SW.png"/>
+ </tile>
+ <tile id="8">
+  <image width="512" height="512" source="junkyard_isometriclandscape/fence_NE.png"/>
+ </tile>
+ <tile id="9">
+  <image width="512" height="512" source="junkyard_isometriclandscape/fence_NW.png"/>
+ </tile>
+ <tile id="10">
+  <image width="512" height="512" source="junkyard_isometriclandscape/fence_SE.png"/>
+ </tile>
+ <tile id="11">
+  <image width="512" height="512" source="junkyard_isometriclandscape/fence_SW.png"/>
+ </tile>
+ <tile id="12">
+  <image width="512" height="512" source="junkyard_isometriclandscape/fenceBroken_NE.png"/>
+ </tile>
+ <tile id="13">
+  <image width="512" height="512" source="junkyard_isometriclandscape/fenceBroken_NW.png"/>
+ </tile>
+ <tile id="14">
+  <image width="512" height="512" source="junkyard_isometriclandscape/fenceBroken_SE.png"/>
+ </tile>
+ <tile id="15">
+  <image width="512" height="512" source="junkyard_isometriclandscape/fenceBroken_SW.png"/>
+ </tile>
+ <tile id="16">
+  <image width="512" height="512" source="junkyard_isometriclandscape/fenceCorner_NE.png"/>
+ </tile>
+ <tile id="17">
+  <image width="512" height="512" source="junkyard_isometriclandscape/fenceCorner_NW.png"/>
+ </tile>
+ <tile id="18">
+  <image width="512" height="512" source="junkyard_isometriclandscape/fenceCorner_SE.png"/>
+ </tile>
+ <tile id="19">
+  <image width="512" height="512" source="junkyard_isometriclandscape/fenceCorner_SW.png"/>
+ </tile>
+ <tile id="20">
+  <image width="512" height="512" source="junkyard_isometriclandscape/fenceCornerCurved_NE.png"/>
+ </tile>
+ <tile id="21">
+  <image width="512" height="512" source="junkyard_isometriclandscape/fenceCornerCurved_NW.png"/>
+ </tile>
+ <tile id="22">
+  <image width="512" height="512" source="junkyard_isometriclandscape/fenceCornerCurved_SE.png"/>
+ </tile>
+ <tile id="23">
+  <image width="512" height="512" source="junkyard_isometriclandscape/fenceCornerCurved_SW.png"/>
+ </tile>
+ <tile id="24">
+  <image width="512" height="512" source="junkyard_isometriclandscape/machine_barrel_NE.png"/>
+ </tile>
+ <tile id="25">
+  <image width="512" height="512" source="junkyard_isometriclandscape/machine_barrel_NW.png"/>
+ </tile>
+ <tile id="26">
+  <image width="512" height="512" source="junkyard_isometriclandscape/machine_barrel_SE.png"/>
+ </tile>
+ <tile id="27">
+  <image width="512" height="512" source="junkyard_isometriclandscape/machine_barrel_SW.png"/>
+ </tile>
+ <tile id="28">
+  <image width="512" height="512" source="junkyard_isometriclandscape/machine_barrelLarge_NE.png"/>
+ </tile>
+ <tile id="29">
+  <image width="512" height="512" source="junkyard_isometriclandscape/machine_barrelLarge_NW.png"/>
+ </tile>
+ <tile id="30">
+  <image width="512" height="512" source="junkyard_isometriclandscape/machine_barrelLarge_SE.png"/>
+ </tile>
+ <tile id="31">
+  <image width="512" height="512" source="junkyard_isometriclandscape/machine_barrelLarge_SW.png"/>
+ </tile>
+ <tile id="32">
+  <image width="512" height="512" source="junkyard_isometriclandscape/machine_generator_NE.png"/>
+ </tile>
+ <tile id="33">
+  <image width="512" height="512" source="junkyard_isometriclandscape/machine_generator_NW.png"/>
+ </tile>
+ <tile id="34">
+  <image width="512" height="512" source="junkyard_isometriclandscape/machine_generator_SE.png"/>
+ </tile>
+ <tile id="35">
+  <image width="512" height="512" source="junkyard_isometriclandscape/machine_generator_SW.png"/>
+ </tile>
+ <tile id="36">
+  <image width="512" height="512" source="junkyard_isometriclandscape/meteor_detailed_NE.png"/>
+ </tile>
+ <tile id="37">
+  <image width="512" height="512" source="junkyard_isometriclandscape/meteor_detailed_NW.png"/>
+ </tile>
+ <tile id="38">
+  <image width="512" height="512" source="junkyard_isometriclandscape/meteor_detailed_SE.png"/>
+ </tile>
+ <tile id="39">
+  <image width="512" height="512" source="junkyard_isometriclandscape/meteor_detailed_SW.png"/>
+ </tile>
+ <tile id="40">
+  <image width="512" height="512" source="junkyard_isometriclandscape/meteor_half_NE.png"/>
+ </tile>
+ <tile id="41">
+  <image width="512" height="512" source="junkyard_isometriclandscape/meteor_half_NW.png"/>
+ </tile>
+ <tile id="42">
+  <image width="512" height="512" source="junkyard_isometriclandscape/meteor_half_SE.png"/>
+ </tile>
+ <tile id="43">
+  <image width="512" height="512" source="junkyard_isometriclandscape/meteor_half_SW.png"/>
+ </tile>
+ <tile id="44">
+  <image width="512" height="512" source="junkyard_isometriclandscape/meteor_NE.png"/>
+ </tile>
+ <tile id="45">
+  <image width="512" height="512" source="junkyard_isometriclandscape/meteor_NW.png"/>
+ </tile>
+ <tile id="46">
+  <image width="512" height="512" source="junkyard_isometriclandscape/meteor_SE.png"/>
+ </tile>
+ <tile id="47">
+  <image width="512" height="512" source="junkyard_isometriclandscape/meteor_SW.png"/>
+ </tile>
+ <tile id="48">
+  <image width="512" height="512" source="junkyard_isometriclandscape/rocks_NE.png"/>
+ </tile>
+ <tile id="49">
+  <image width="512" height="512" source="junkyard_isometriclandscape/rocks_NW.png"/>
+ </tile>
+ <tile id="50">
+  <image width="512" height="512" source="junkyard_isometriclandscape/rocks_SE.png"/>
+ </tile>
+ <tile id="51">
+  <image width="512" height="512" source="junkyard_isometriclandscape/rocks_SW.png"/>
+ </tile>
+</tileset>
