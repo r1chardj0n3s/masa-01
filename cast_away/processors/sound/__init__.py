9230c9d8a37617fea8412ad6d58a127b4db12714
Travis Dixon <travis@growdata.com.au>
2020-09-25 16:55:17 +1000

    rumble move

--- cast_away/processors/sound/__init__.py
+++ cast_away/processors/sound/__init__.py
@@ -1,11 +1,16 @@
 import esper
 import arcade
-
+from cast_away.components.player import Player
+from cast_away.components.velocity import Velocity
 from cast_away.components.sound import Sound
 from cast_away.components.timeout import Timeout
 
+PLAYER_RUMBLE = ":resources:sounds/hit2.wav"
 
 class SoundProcessor(esper.Processor):
+    def __init__(self):
+        self.rumble = arcade_sound = arcade.Sound(PLAYER_RUMBLE, streaming=True)
+
     def process(self, dt):
         for ent, sound in self.world.get_component(Sound):
             if self.world.has_component(ent, Timeout):
@@ -13,6 +18,12 @@ class SoundProcessor(esper.Processor):
             arcade_sound = arcade.Sound(sound.file_path)
             arcade_sound.play(volume=sound.volume)
             self.world.delete_entity(ent)
+
+        for ent, (player, velocity) in self.world.get_components(Player, Velocity):
+            if velocity.magnitude > 0:
+                self.rumble.play(volume=0.003)
+            else:
+                self.rumble.stop()
 
 
 def init(world):
