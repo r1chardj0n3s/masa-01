3b6a2a7ee211b881cfbeb4794acca7b4c00093d7
Travis Dixon <travis@growdata.com.au>
2020-09-25 21:36:21 +1000

    package executable

---
+++ build.bat
@@ -0,0 +1,15 @@
+@echo off
+for /f %%i in ('python -c "import site; print(site.getsitepackages()[0])"') do set PYTHON_ROOT="%%i"
+pyinstaller ^
+ --clean ^
+ --exclude-module tkinter ^
+ --add-data %PYTHON_ROOT%\Lib\site-packages\pymunk\chipmunk.dll;. ^
+ --add-data %PYTHON_ROOT%\src\arcade\arcade\soloud\soloud_*.dll;. ^
+ --add-data %PYTHON_ROOT%\src\arcade\arcade\resources;arcade\resources ^
+ --add-data "./data;data" ^
+ -n cast_away_robot ^
+ -c ^
+ -d all ^
+ run_game.py
+
+pause
