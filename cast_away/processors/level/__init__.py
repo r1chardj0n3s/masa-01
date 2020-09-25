cc772198544147d85faf075d7666be807da4921c
Travis Dixon <travis@growdata.com.au>
2020-09-25 16:59:52 +1000

    level swap sound

--- cast_away/processors/level/__init__.py
+++ cast_away/processors/level/__init__.py
@@ -7,10 +7,10 @@ from cast_away.components.useful_polygon import UsefulPolygon
 from cast_away.components.position import Position
 from cast_away.components.player import Player
 from cast_away.components.level.player_spawn import PlayerSpawns
-from cast_away.components.level import LevelProgression, Level
+from cast_away.components.level import LevelProgression, Level, LEVEL_SWAP_SOUND
 
 from cast_away.entities.level import activate_map, deactivate_map
-
+from cast_away.entities.sound import create_sound
 
 def map_filename(name):
     return f"data/{name}.tmx"
@@ -24,6 +24,7 @@ class LevelProcessor(esper.Processor):
                     deactivate_map(self.world, current_level.level_ent)
                 activate_map(self.world, current_level)
                 current_level.next_level = None
+                create_sound(self.world, LEVEL_SWAP_SOUND)
 
             level_ent = current_level.level_ent
 
