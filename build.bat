653d6fd640e18d95eee1d18f62bd152ad03036ec
Travis Dixon <travis@growdata.com.au>
2020-09-26 07:43:07 +1000

    make pyinstaller work

--- build.bat
+++ build.bat
@@ -4,7 +4,7 @@ pyinstaller ^
  --clean ^
  --exclude-module tkinter ^
  --add-data %PYTHON_ROOT%\Lib\site-packages\pymunk\chipmunk.dll;. ^
- --add-data %PYTHON_ROOT%\src\arcade\arcade\soloud\soloud_*.dll;. ^
+ --add-data %PYTHON_ROOT%\src\arcade\arcade\soloud\soloud_*.dll;arcade\soloud ^
  --add-data %PYTHON_ROOT%\src\arcade\arcade\resources;arcade\resources ^
  --add-data "./data;data" ^
  -n cast_away_robot ^
