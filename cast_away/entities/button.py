948f6e17f96d7fb33fb648ef075083c039be0c55
Travis Dixon <travis@growdata.com.au>
2020-09-25 20:58:33 +1000

    button not triggered by enemy

--- cast_away/entities/button.py
+++ cast_away/entities/button.py
@@ -1,11 +1,26 @@
+from pydoc import locate
+
 from cast_away.components.button import Button
 from cast_away.components.position import Position
 from cast_away.components.collidable import Collidable, HitCircle
 from cast_away.components.sprite import Sprite
 
+def locate_classes(properties, key):
+    classnames = properties.get(key, "")
+    if classnames == "":
+        return []
+    classnamelist = classnames.split(",")
+    return [locate(name) for name in classnamelist]
+
 def create_button(world, obj, level_ent):
-    channel = obj.properties.get("channel")
-    in_level = obj.properties.get("in_level", False)
+    properties = obj.properties
+    if properties is None:
+        properties = {}
+    channel = properties.get("channel")
+    in_level = properties.get("in_level", False)
+
+    match_components = locate_classes(properties, "match_components")
+    avoid_components = locate_classes(properties, "avoid_components")
     up_image = "data/button_up_sprite.png"
     down_image = "data/button_down_sprite.png"
     return world.create_entity(
@@ -20,7 +35,10 @@ def create_button(world, obj, level_ent):
             y = obj.location.y, 
             level = level_ent
         ),
-        Collidable(),
+        Collidable(
+            match_components=match_components,
+            avoid_components=avoid_components
+        ),
         HitCircle(20),
         Sprite(up_image, scale=0.5),
     )
