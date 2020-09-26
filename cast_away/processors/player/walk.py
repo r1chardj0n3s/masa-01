d887d9020006c7820510513548e618361383bd2e
Travis Dixon <travis@growdata.com.au>
2020-09-26 18:29:23 +1000

    update to player walk sound

--- cast_away/processors/player/walk.py
+++ cast_away/processors/player/walk.py
@@ -1,9 +1,17 @@
 import esper
+import arcade
 from cast_away.components.player import Player
 from cast_away.components.velocity import Velocity
 from cast_away.components.sprite_effect import SpriteEffects, TwistEffect
 
+
+PLAYER_WALK_SOUND = "data/sound/Castaway_move_3.wav"
+
 class WalkProcessor(esper.Processor):
+    def __init__(self):
+        self.walk_sound = arcade.Sound(PLAYER_WALK_SOUND)
+        self.walk_sound.play(volume=0)
+
     def process(self, dt):
         for ent, (player, velocity) in self.world.get_components(Player, Velocity):
             if velocity.magnitude != 0:
@@ -20,6 +28,11 @@ class WalkProcessor(esper.Processor):
                         play_time=0.2, 
                         size=.005
                     ))
+
+                if not self.walk_sound.is_complete() == 0:
+                    self.walk_sound.play(volume=0.05)
+            else:
+                self.walk_sound.stop()
 
 
 def init(world):
--- cast_away/processors/sound/__init__.py
+++ cast_away/processors/sound/__init__.py
@@ -5,14 +5,8 @@ from cast_away.components.velocity import Velocity
 from cast_away.components.sound import Sound
 from cast_away.components.timeout import Timeout
 
-PLAYER_RUMBLE = ":resources:sounds/hit2.wav"
-
 
 class SoundProcessor(esper.Processor):
-    def __init__(self):
-        self.rumble = arcade.Sound(PLAYER_RUMBLE, streaming=True)
-        self.playing = False
-
     def process(self, dt):
         for ent, sound in self.world.get_component(Sound):
             if self.world.has_component(ent, Timeout):
@@ -20,15 +14,6 @@ class SoundProcessor(esper.Processor):
             arcade_sound = arcade.Sound(sound.file_path)
             arcade_sound.play(volume=sound.volume)
             self.world.delete_entity(ent)
-
-        for ent, (player, velocity) in self.world.get_components(Player, Velocity):
-            if velocity.magnitude > 0 and not self.playing:
-                self.rumble.play(volume=0.001)
-                self.playing = True
-            else:
-                self.rumble.stop()
-                self.playing = False
-
 
 def init(world):
     world.add_processor(SoundProcessor())
