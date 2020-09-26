0af109cd0cded3a4a9851a28dd43fc8834ed8c24
Travis Dixon <travis@growdata.com.au>
2020-09-26 10:41:16 +1000

    star firework

--- cast_away/graphics/emitters.py
+++ cast_away/graphics/emitters.py
@@ -1,6 +1,6 @@
 import arcade
 import random
-
+from cast_away.graphics.particles import AccelleratingParticle
 
 def bee_poof(position):
     return arcade.Emitter(
@@ -17,3 +17,24 @@ def bee_poof(position):
         scale=0.1 * random.random(),
     ),
 )
+
+
+FIREWORK_PARTICLE = ":resources:images/items/star.png"
+
+
+def firework(position):
+    x = position.x
+    y = position.y
+    
+    return arcade.Emitter(
+        center_xy=(x, y),
+        change_xy=(0, 5),
+        emit_controller=arcade.EmitterIntervalWithTime(emit_interval=0.1, lifetime=3),
+        particle_factory=lambda emitter: arcade.FadeParticle(
+            filename_or_texture=FIREWORK_PARTICLE,
+            change_xy=arcade.rand_in_circle((0.0, 0.0), 1.0),
+            lifetime=1.5,
+            scale=0.5 * random.random(),
+        ),
+    )
+
