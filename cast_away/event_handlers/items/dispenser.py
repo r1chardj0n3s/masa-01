ff6f42d3aa1741574f2f23b0de9d0040453b52e5
Travis Dixon <travis@growdata.com.au>
2020-09-26 18:48:25 +1000

    more tactile trash cans

--- cast_away/event_handlers/items/dispenser.py
+++ cast_away/event_handlers/items/dispenser.py
@@ -2,7 +2,11 @@ from cast_away.event_dispatch import register_listener, COLLISION
 from cast_away.components.items.dispenser import Dispenser
 from cast_away.components.inventory import Inventory
 from cast_away.components.items import InventoryItem
+from cast_away.components.sprite_effect import SpriteEffects, ScaleUpDownEffect
 from cast_away.entities.item import create_inventory_item
+from cast_away.entities.sound import create_sound
+from cast_away.event_handlers.items.pickup import PICKUP_SOUND
+
 
 def collide(world, message):
     dispenser_ent, inventory_ent = message.payload
@@ -14,6 +18,9 @@ def collide(world, message):
                 if item_name == dispenser.item_name:
                     return
         create_inventory_item(world, dispenser_ent, inventory_ent, dispenser.item_name)
+        world.add_component(dispenser_ent, SpriteEffects(ScaleUpDownEffect(play_time=0.3, to_scale=0.8)))
+        create_sound(world, PICKUP_SOUND)
+        
 
 def init():
     register_listener(COLLISION, collide)
