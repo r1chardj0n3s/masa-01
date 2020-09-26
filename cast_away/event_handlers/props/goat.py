bf24c4233206f9b4555ba8e03f2ae64f18beb9f2
Richard Jones <r1chardj0n3s@gmail.com>
2020-09-26 18:40:35 +1000

    IT IS PERFECT NOW

--- cast_away/event_handlers/props/goat.py
+++ cast_away/event_handlers/props/goat.py
@@ -3,7 +3,7 @@ from cast_away.event_dispatch import (
     COLLISION,
 )
 
-# from cast_away.entities.sound import create_sound
+from cast_away.entities.sound import create_sound
 from cast_away.components.sprite_effect import ScaleUpDownEffect, SpriteEffects
 from cast_away.components.prop import Prop
 from cast_away.components.timeout import Timeout
@@ -16,8 +16,9 @@ def collision(world, message):
             if world.has_component(goat, Timeout):
                 return
             effects = world.component_for_entity(goat, SpriteEffects)
-            effects.effects.append(ScaleUpDownEffect(.45))
-            world.add_component(goat, Timeout(.5))
+            effects.effects.append(ScaleUpDownEffect(0.45))
+            world.add_component(goat, Timeout(0.5))
+            create_sound(world, "data/sound/344057__reitanna__why2.wav")
 
 
 def init():
